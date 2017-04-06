48403f6d3d7e7a73ffe255efb31e31076ab44f9b
Tal Schuster <talschuster@localhost.localdomain>
2017-04-06 14:35:16 +0300

    initial files

---
+++ ngram_model.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python
+
+from data_utils import utils as du
+import numpy as np
+import pandas as pd
+import csv
+
+# Load the vocabulary
+vocab = pd.read_table("data/lm/vocab.ptb.txt", header=None, sep="\s+",
+                     index_col=0, names=['count', 'freq'], )
+
+# Choose how many top words to keep
+vocabsize = 2000
+num_to_word = dict(enumerate(vocab.index[:vocabsize]))
+word_to_num = du.invert_dict(num_to_word)
+
+# Load the training set
+docs_train = du.load_dataset('data/lm/ptb-train.txt')
+S_train = du.docs_to_indices(docs_train, word_to_num)
+docs_dev = du.load_dataset('data/lm/ptb-dev.txt')
+S_dev = du.docs_to_indices(docs_dev, word_to_num)
+
+def train_ngrams(dataset):
+    """
+        Gets an array of arrays of indexes, each one corresponds to a word.
+        Returns trigram, bigram, unigram and total counts.
+    """
+    trigram_counts = dict()
+    bigram_counts = dict()
+    unigram_counts = dict()
+    token_count = 0
+    import ipdb; ipdb.set_trace()
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+    return trigram_counts, bigram_counts, unigram_counts, token_count
+
+def evaluate_ngrams(eval_dataset, trigram_counts, bigram_counts, unigram_counts, train_token_count, lambda1, lambda2):
+    """
+    Goes over an evaluation dataset and computes the perplexity for it with
+    the current counts and a linear interpolation
+    """
+    perplexity = 0
+    ### YOUR CODE HERE
+    raise NotImplementedError
+    ### END YOUR CODE
+    return perplexity
+
+def test_ngram():
+    """
+    Use this space to test your n-gram implementation.
+    """
+    #Some examples of functions usage
+    trigram_counts, bigram_counts, unigram_counts, token_count = train_ngrams(S_train)
+    print "#trigrams: " + str(len(trigram_counts))
+    print "#bigrams: " + str(len(bigram_counts))
+    print "#unigrams: " + str(len(unigram_counts))
+    print "#tokens: " + str(token_count)
+    perplexity = evaluate_ngrams(S_dev, trigram_counts, bigram_counts, unigram_counts, token_count, 0.5, 0.4)
+    print "#perplexity: " + str(perplexity)
+    ### YOUR CODE HERE
+    ### END YOUR CODE
+
+if __name__ == "__main__":
+    test_ngram()
